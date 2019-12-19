fa4a529f88f97e991378a4f7020a77f2b028d233
= <alejandrolistc@hotmail.com>
2019-12-18 22:26:51 -0500

    utilizando la misma configuracion de vim para nvim

--- installplugvim.sh
+++ installplugvim.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/bash
+mkdir -p ~/.config/nvim
+mkdir -p ~/.vim/plugged
+cp init.vim ~/.vim/plugged/
+
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim

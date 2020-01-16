8a2fc095bc89448b037b085079add09e0366ed05
Alelizzt <alejandrolistc@hotmail.com>
2020-01-15 23:41:10 -0500

    Zsh for linux

---
+++ installzsh.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+sudo apt install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+

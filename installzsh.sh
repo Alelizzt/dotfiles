16ecb387405a2fa4c714687de63497234d1bd309
root <root@localhost.localdomain>
2020-01-24 17:09:12 -0500

    no root here

--- installzsh.sh
+++ installzsh.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/bash
+#evitando instalar cosas con root
+if [[ $EUID == 0 ]]; then
+    echo "Eres root, no puedes ejecutar este script" 1>&2
+    exit 1
+fi
+
 #instalando oh-my-zsh
 sudo apt install zsh -y
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 #instalando tema
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
@@ -15,15 +21,8 @@ mv zsh-syntax-highlighting .oh-my-zsh/custom/plugins/
 
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 
-sudo chown root:root -R zsh-autosuggestions
-sudo chown root:root -R zsh-completions
-sudo chown root:root -R zsh-syntax-highlighting
-
-
-
 sudo dpkg -i lsd_0.14.0_amd64.deb
-cp .zshrc ~/.zshrc
-sudo dpkg -i ripgrep_11.0.2_amd64.deb
+#cp .zshrc ~/.zshrc
 
 #ctrl+t busqueda recursiva
 # comando ctrl+r
@@ -32,7 +31,7 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 ##para aplicar en root tambien
-sudo su
-ln -s -f /home/[YOURUSER]/.zshrc .zshrc
+#sudo su
+#ln -s -f /home/[YOURUSER]/.zshrc .zshrc
 
 

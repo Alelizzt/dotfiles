991baf503a74903d5c327dde6a43445eb1d55470
Alelizzt <alejandrolistc>
2022-01-01 12:53:07 -0500

    Reset dotfiles

--- configtmux.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-cp -r .tmux ~/.tmux
-ln -s ~/.tmux/.tmux.conf ~/.tmux.conf
-cp ~/.tmux/.tmux.conf.local ~/.tmux.conf.local
--- wifi_status.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
- 
-echo " $(/usr/sbin/ifconfig wlan0 | grep "inet " | awk '{print $2}')"
-

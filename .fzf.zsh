ceb77ad25e306199dbbb260c8a719dd45adf12c4
Alelizzt <alejandrolistc>
2022-01-01 13:48:17 -0500

    fzf configuration

---
+++ .fzf.zsh
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/alelizzt/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/alelizzt/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/alelizzt/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/alelizzt/.fzf/shell/key-bindings.zsh"
